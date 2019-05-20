249eef04ab1228004d677d0c865d6d76c83fb0ab
MAllen07 <mrsmallen07@gmail.com>
2019-05-20 19:17:16 -0400

    answering questions - working on get request on serverjs

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const server = express();
 
+Foobar.get('data/accounts-model.js', (req, res) => {
+    return res.send('access to new database')
+}
+)
+
 // your code here
 
 module.exports = server;
