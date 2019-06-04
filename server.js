0938ba728c2de2d0e01245386cf8b2349e6f8ccb
MAllen07 <mrsmallen07@gmail.com>
2019-06-04 16:53:03 -0400

    added helmet and morgan to pkgejson - no issues showing on server

--- server.js
+++ server.js
@@ -13,27 +13,27 @@ server.use(morgan("dev"));
 // your code here
 
 server.get("/", async (req, res) => {
-    try {
-        const data = await accounts.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "error" });
-    }
+  try {
+    const data = await accounts.find();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
 });
 
 server.get("/:id", async (req, res) => {
-    try {
-        const data = await accounts.findById(req.params.id);
-        if (data) {
-            res.json(data);
-        } else {
-            res.status(404).json({ message: "error" });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "error" });
+  try {
+    const data = await accounts.findById(req.params.id);
+    if (data) {
+      res.json(data);
+    } else {
+      res.status(404).json({ message: "error" });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
 });
 
 module.exports = server;
